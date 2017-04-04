905dc070164318f4f26b9cc94b587c279b2cd035
Brendan Walker <brenny_walker@hotmail.com>
2017-04-04 20:20:53 +0000

    Adds a loop to display user messages

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 
 class Chatbar extends React.Component {
   render() {
-    console.log('Rendering <ChatBar/>');
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-username" placeholder={this.props.username.name} />
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </footer>
     );
